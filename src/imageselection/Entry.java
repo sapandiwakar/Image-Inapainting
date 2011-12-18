79a2fce2ce22ee0239793fe902fafdea7229355a
Sapan Diwakar <diwakar.sapan@gmail.com>
2011-12-18 12:21:20 +0100

    Update src/imageselection/Entry.java

--- src/imageselection/Entry.java
+++ src/imageselection/Entry.java
@@ -8,7 +8,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
  * copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>. Please
  * contact Sapan Diwakar or Pulkit Goyal diwakar.sapan@gmail.com or pulkit110@gmail.com or visit
- * <http://sapandiwakar.wordpress.com> or <http://pulkitgoyal.wordpress.com> if you need any additional information or
+ * <http://sapandiwakar.in> or <http://pulkitgoyal.in> if you need any additional information or
  * have any questions.
  */
 
